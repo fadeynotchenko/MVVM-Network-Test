b4b08061970ca526c18f62c417ca8eb12275e014
fadeynotchenko <notchenkofaddey@gmail.com>
2022-01-31 20:58:25 +0300

    Initial Commit

---
+++ MVVM Network Test Project/MVVM_Network_Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_Network_Test_ProjectApp.swift
+//  MVVM Network Test Project
+//
+//  Created by Fadey Notchenko on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_Network_Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
